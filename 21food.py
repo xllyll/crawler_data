8e61972c6d98e273a1c1934ca937227cfa3a0af4
xllyll <757271398@qq.com>
2025-02-25 17:51:00 +0800

    FIX

--- 21food.py
+++ 21food.py
@@ -1,4 +1,5 @@
 # -*- codeing = utf-8 -*-
+from selenium import webdriver
 from bs4 import BeautifulSoup  # 网页解析，获取数据
 import re  # 正则表达式，进行文字匹配`
 import urllib.request, urllib.error  # 制定URL，获取网页数据
@@ -17,7 +18,7 @@ findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
 
 
 def main():
-    baseurl = "https://movie.douban.com/top250?start="  #要爬取的网页链接
+    baseurl = "https://price.21food.cn/market-p2.html"  #要爬取的网页链接
     # 1.爬取网页
     datalist = getData(baseurl)
     savepath = "豆瓣电影Top250.xls"    #当前目录新建XLS，存储进去
@@ -30,12 +31,29 @@ def main():
 
 # 爬取网页
 def getData(baseurl):
+
+
+
+    # 启动无头浏览器
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # 无头模式
+    driver = webdriver.Chrome(options=options)
+
+    # 打开目标网页
+    driver.get(baseurl)
+
+    # 获取渲染后的页面内容
+    content = driver.page_source
+    print(content)
+
+
     datalist = []  #用来存储爬取的网页信息
     for i in range(0, 10):  # 调用获取页面信息的函数，10次
-        url = baseurl + str(i * 25)
+        url = baseurl #str(i * 25)
         html = askURL(url)  # 保存获取到的网页源码
         # 2.逐一解析数据
         soup = BeautifulSoup(html, "html.parser")
+        sjs_top_cent_erv = soup.find_all('div',class_="sjs_top_cent_erv")
         for item in soup.find_all('div', class_="item"):  # 查找符合要求的字符串
             data = []  # 保存一部电影所有信息
             item = str(item)
@@ -74,7 +92,7 @@ def getData(baseurl):
 # 得到指定一个URL的网页内容
 def askURL(url):
     head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
-        "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
     }
     # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接收什么水平的文件内容）
 
